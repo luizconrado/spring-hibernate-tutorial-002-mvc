54a3a60084178795dd6b12535be4acc72b970eab
Luiz Felipe Conrado <lfconrado@gmail.com>
2020-07-29 01:18:38 +0200

    2020-07-29 01:30

---
+++ src/main/java/com/luizconrado/springmvc/configuration/MyLoggerConfig.java
@@ -0,0 +1,49 @@
+package main.java.com.luizconrado.springmvc.configuration;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class MyLoggerConfig {
+
+
+    private String rootLoggerLevel;
+    private String printedLoggerLevel;
+
+    public void setRootLoggerLevel(String rootLoggerLevel) {
+        this.rootLoggerLevel = rootLoggerLevel;
+    }
+
+    public void setPrintedLoggerLevel(String printedLoggerLevel) {
+        this.printedLoggerLevel = printedLoggerLevel;
+    }
+
+    public void initLogger() {
+
+        // parse levels
+        Level rootLevel = Level.parse(rootLoggerLevel);
+        Level printedLevel = Level.parse(printedLoggerLevel);
+
+        // get logger for app context
+        Logger applicationContextLogger = Logger.getLogger(org.springframework.web.servlet.DispatcherServlet.class.getName());
+
+        // not used, but I don't know why
+        // Logger applicationContextLogger2 = Logger.getLogger(ClassPathXmlApplicationContext.class.getName());
+
+        // get parent logger
+        Logger loggerParent = applicationContextLogger.getParent();
+
+        // set root logging level
+        loggerParent.setLevel(rootLevel);
+
+        // set up console handler
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(printedLevel);
+        consoleHandler.setFormatter(new SimpleFormatter());
+
+        // add handler to the logger
+        loggerParent.addHandler(consoleHandler);
+    }
+
+}
--- src/main/java/com/luizconrado/springmvc/controllers/FirstController.java
+++ src/main/java/com/luizconrado/springmvc/controllers/FirstController.java
@@ -1,5 +1,6 @@
 package main.java.com.luizconrado.springmvc.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
+//    private static final Logger logger = Logger.getLogger(FirstController.class);
+
+
     @GetMapping("/")
     public String index(Model model) {
+
         System.out.println("I reached the @GetMapping(\"/\")");
         model.addAttribute("message", "Hello Spring MVC 5!");
         return "index";
