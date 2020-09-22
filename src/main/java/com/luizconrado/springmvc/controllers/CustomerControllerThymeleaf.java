fb0c06efa3426f6dc8ccba12e79fef0bfd4f538f
Luiz Felipe Conrado <lfconrado@gmail.com>
2020-09-22 17:50:30 +0200

    2020-09-22 17:50

--- src/main/java/com/luizconrado/springmvc/controllers/CustomerControllerThymeleaf.java
+++ src/main/java/com/luizconrado/springmvc/controllers/CustomerControllerThymeleaf.java
@@ -1,11 +1,48 @@
 package main.java.com.luizconrado.springmvc.controllers;
 
+import main.java.com.luizconrado.springmvc.model.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/thymeleaf/customer")
 public class CustomerControllerThymeleaf {
 
+
+    @RequestMapping(path = "showForm", method = {RequestMethod.GET})
+    public String showForm(Model theModel) {
+
+        // create a new customer object
+        Customer customer = new Customer();
+
+        // add customer object to the model
+        theModel.addAttribute("customer", customer);
+
+
+        return "templates/customer-form";
+    }
+
+
+    @RequestMapping(path = "processForm", method = {RequestMethod.POST})
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+
+        // log the customer data
+        System.out.println("customer: " + customer);
+
+        if (bindingResult.hasErrors()) {
+            return "views/customer-form";
+
+        } else {
+            return "templates/customer-confirmation";
+        }
+
+
+    }
 
 }
